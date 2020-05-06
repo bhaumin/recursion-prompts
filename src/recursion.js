78f4ba2d16aa2a3813b601b998cf0745acef26d1
Bhaumin Shah <bhaumin@gmail.com>
2020-05-07 02:17:30 +0530

    Implement powerOfTwo and reverse

--- src/recursion.js
+++ src/recursion.js
@@ -111,10 +111,24 @@ var exponent = function(base, exp) {
 // powerOfTwo(16); // true
 // powerOfTwo(10); // false
 var powerOfTwo = function(n) {
+  if (n === 1) {
+    return true;
+  }
+
+  if (n === 0 || n % 2 !== 0) {
+    return false;
+  }
+
+  return powerOfTwo(Math.floor(n / 2));
 };
 
 // 9. Write a function that reverses a string.
 var reverse = function(string) {
+  if (string.length <= 1) {
+    return string;
+  }
+
+  return reverse(string.slice(1)) + string[0];
 };
 
 // 10. Write a function that determines if a string is a palindrome.
