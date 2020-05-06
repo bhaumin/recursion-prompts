b5a51d122f48448825883c7867ea42b22adc10ee
Bhaumin Shah <bhaumin@gmail.com>
2020-05-07 02:30:44 +0530

    Implement palindrome

--- src/recursion.js
+++ src/recursion.js
@@ -133,6 +133,15 @@ var reverse = function(string) {
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
+  if (string.length <= 1) {
+    return true;
+  } else if (string.length === 2) {
+    return string[0].toLowerCase() === string[1].toLowerCase();
+  } else if (string[0].toLowerCase() !== string[string.length - 1].toLowerCase()) {
+    return false;
+  }
+
+  return palindrome(string.slice(1, -1));
 };
 
 // 11. Write a function that returns the remainder of x divided by y without using the
